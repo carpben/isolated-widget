b0eea618176cb299c0659fb911b82412d099648b
ben-thinkpad-e480 <ben@givingway.com>
2018-11-17 14:45:16 +0200

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
 import { Component, h } from 'preact'
-// import { css, keyframes } from "preact-emotion"
 import DonateBtn from "./DonateBtn"
 import { css, IsolatedApp, keyframes } from "./isolatedEmotion"
-
-
-
-const move = keyframes`
-  0% {
-	 bottom: -50px;
-  }
-  50% {
-	  bottom: 35px; 
-  }
-      
-`
-
-
-const Box = css`
-	position: fixed; 
-	bottom: 15px; 
-	right: 15px; 
-	z-index: 9999999999999!important;
-	font-family: Arial, Helvetica, sans-serif;
-	animation: ${move} 0.3s ease-in-out 2s; 
-	animation-fill-mode: both;
-`
 
 class App extends Component {
 	public render() {
@@ -38,8 +14,24 @@ class App extends Component {
 	}
 }
 
+const move = keyframes`
+  0% {
+	 bottom: -50px;
+  }
+  50% {
+	  bottom: 35px; 
+  }
+      
+`
 
-
-
+const Box = css`
+	position: fixed; 
+	bottom: 15px; 
+	right: 15px; 
+	z-index: 9999999999999!important;
+	font-family: Arial, Helvetica, sans-serif;
+	animation: ${move} 0.3s ease-in-out 2s; 
+	animation-fill-mode: both;
+`
 
 export default App;
